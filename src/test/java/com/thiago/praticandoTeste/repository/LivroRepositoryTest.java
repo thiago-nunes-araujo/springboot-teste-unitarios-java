2cddb8da8ea9e6f9c4fdb374ba47456ee156018b
thiago.araujo <thiago.araujo@db1.com.br>
2020-07-17 16:57:38 -0400

    #3: Ajustando versao do JUnit e Ajuste nos test implementados

--- src/test/java/com/thiago/praticandoTeste/repository/LivroRepositoryTest.java
+++ src/test/java/com/thiago/praticandoTeste/repository/LivroRepositoryTest.java
@@ -3,43 +3,23 @@ package com.thiago.praticandoTeste.repository;
 import com.thiago.praticandoTeste.model.Autor;
 import com.thiago.praticandoTeste.model.Livro;
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class LivroRepositoryTest {
 
     @Autowired
     private LivroRepository livroRepository;
 
-    @Before
-    public void beforeTest(){
-        Livro primeiroLIvro = Livro.builder()
-                .titulo("Java 8")
-                .data_publicacao(LocalDate.now())
-                .autor(Autor.builder().nome("JOsehp Lomdrow").idade(87).build())
-                .build();
-
-        Livro segundoLivro = Livro.builder()
-                .titulo("Logica de programação")
-                .data_publicacao(LocalDate.of(1999, 8, 20))
-                .autor(Autor.builder().nome("Crystian Buller").idade(86).build())
-                .build();
-
-        List<Livro> livros = this.livroRepository.saveAll(Arrays.asList(primeiroLIvro, segundoLivro));
-        livros.size();
-    }
-
     @Test
+    @DisplayName("should created a book")
     public void TestCreateLivro(){
         Livro livro = Livro.builder()
                 .titulo("Java 8")
@@ -54,29 +34,70 @@ public class LivroRepositoryTest {
     }
 
     @Test
+    @DisplayName("should return a List book especific size")
     public void shouldReturnSizeListLivrosEqualsTwo(){
+        Livro primeiroLIvro = Livro.builder()
+                .titulo("Java 8")
+                .data_publicacao(LocalDate.now())
+                .autor(Autor.builder().nome("JOsehp Lomdrow").idade(87).build())
+                .build();
+
+        Livro segundoLivro = Livro.builder()
+                .titulo("Logica de programação")
+                .data_publicacao(LocalDate.of(1999, 8, 20))
+                .autor(Autor.builder().nome("Crystian Buller").idade(86).build())
+                .build();
+
+        this.livroRepository.saveAll(Lists.newArrayList(primeiroLIvro, segundoLivro));
+
         List<Livro> livros = this.livroRepository.findAll();
         Assertions.assertThat(livros.size()).isEqualTo(2);
     }
 
     @Test
-    public void shouldAutorWithIdIsNotNull() {
+    @DisplayName("should return a autor persistent with book")
+    public void shouldReturnAutorWithIdIsNotNull() {
+        Livro livroNew = Livro.builder()
+                .titulo("Logica de programação")
+                .data_publicacao(LocalDate.of(1999, 8, 20))
+                .autor(Autor.builder().nome("Crystian Buller").idade(86).build())
+                .build();
+
+        this.livroRepository.save(livroNew);
+
         List<Livro> livros = this.livroRepository.findAll();
         Assertions.assertThat(livros.get(0).getAutor().getId()).isNotNull();
-        Assertions.assertThat(livros.get(1).getAutor().getId()).isNotNull();
     }
 
     @Test
-    public void TestDeleteLivro() {
+    @DisplayName("should delete a book")
+    public void shouldDeleteOneBook() {
+        Livro livroNew = Livro.builder()
+                .titulo("Logica de programação")
+                .data_publicacao(LocalDate.of(1999, 8, 20))
+                .autor(Autor.builder().nome("Crystian Buller").idade(86).build())
+                .build();
+
+        this.livroRepository.save(livroNew);
+
         Livro livro = this.livroRepository.findByTitulo("Logica de programação");
         this.livroRepository.delete(livro);
 
         List<Livro> livros = this.livroRepository.findAll();
-        Assertions.assertThat(livros.size()).isEqualTo(1);
+        Assertions.assertThat(livros.size()).isEqualTo(0);
     }
 
     @Test
-    public void TestPutLivro(){
+    @DisplayName("should update a book")
+    public void shouldUpdateOneBook() {
+        Livro segundoLivro = Livro.builder()
+                .titulo("Logica de programação")
+                .data_publicacao(LocalDate.of(1999, 8, 20))
+                .autor(Autor.builder().nome("Crystian Buller").idade(86).build())
+                .build();
+
+        this.livroRepository.save(segundoLivro);
+
         Livro livro = this.livroRepository.findByTitulo("Logica de programação");
         livro.setTitulo("Orientação a objeto 2");
 
